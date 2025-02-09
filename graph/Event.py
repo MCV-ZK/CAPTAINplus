89229b011d75933e56b3e758ec7d46bf57effd91
Zhengkai Wang <127574726+MCV-ZK@users.noreply.github.com>
2025-02-09 21:28:27 +0800

    Create Event.py

---
+++ graph/Event.py
@@ -0,0 +1,59 @@
+import json
+from policy.floatTags import citag
+import numpy as np
+import re
+
+class Event:
+    def __init__(self, id, ts):
+        self.id = id
+        self.time = ts
+        self.type = None
+        self.parameters = None
+        self.src = None
+        self.dest = None
+        self.dest2 = None
+        self.obj_path = None
+
+    def dumps(self) -> str:
+        json_dict = {}
+        json_dict['id'] = self.id
+        json_dict['time'] = self.time
+        json_dict['type'] = self.type
+        json_dict['params'] = self.parameters
+        json_dict['s'] = self.src
+        json_dict['d'] = self.dest
+        json_dict['d2'] = self.dest2
+        return json.dumps(json_dict)
+    
+    def __str__(self):
+        return self.dumps()
+
+    def loads(self, data_str):
+        json_dict = json.loads(data_str)
+        self.type = json_dict['type']
+        if self.type == "UPDATE":
+            self.value = json_dict['value']
+            self.nid = json_dict['nid']
+        else:
+            self.id = json_dict['id']
+            self.time = json_dict['time']
+            self.parameters = json_dict['params']
+            self.src = json_dict['s']
+            self.dest = json_dict['d']
+            self.dest2 = json_dict['d2']
+
+    def load_from_dict(self, json_dict):
+        self.type = json_dict['type']
+        if self.type == "UPDATE":
+            self.value = json_dict['value']
+            self.nid = json_dict['nid']
+        elif self.type == "OBJECT_VERSION_UPDATE":
+            self.old = json_dict['old']
+            self.new = json_dict['new']
+        else:
+            self.id = json_dict['id']
+            self.time = json_dict['time']
+            self.parameters = json_dict['params']
+            self.src = json_dict['s']
+            self.dest = json_dict['d']
+            self.dest2 = json_dict['d2']
